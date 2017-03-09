7861ba73e39e17feb985386a35ff6318ed8bb92e
wind <aa.veter@gmail.com>
2017-03-09 20:47:34 +0300

    first imports

--- main.py
+++ main.py
@@ -5,6 +5,15 @@
 Main program file.
 
 '''
+from api.NetAPI import NetInterface
+from data.main import Data
+#from gui import SomeGui ## ???
 
+def main():
+    print('''
 
+Hello! I am the game - @Sea battle@
+    by python curs 2016-2017. IFMO.
+
+    ''')
 
